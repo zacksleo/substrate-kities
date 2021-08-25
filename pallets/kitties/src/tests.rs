180d4bde4a2cc89ffd81d3802c73d4511c464d71
zacksleo <zacksleo@gmail.com>
2021-08-25 14:45:02 +0800

    tests(kitties): refact tests with macro and helper function

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,22 +1,37 @@
 use super::*;
 use crate::mock::{new_test_ext, Event as TestEvent, Kitties, Origin, System, Test};
 use crate::Error;
+use frame_support::dispatch::DispatchResult;
 use frame_support::{assert_noop, assert_ok};
+
+/// Assert the given `event` exists.
+///
+/// Used as `assert_event!(Event::KittyCreated(1, 1))`,
+macro_rules! assert_event {
+	($x:expr) => {
+		System::assert_has_event(TestEvent::Kitties($x))
+	};
+}
+
+/// create new kitty
+fn new_kitty(account_id: u64) -> DispatchResult {
+	Kitties::create(Origin::signed(account_id))
+}
 
 #[test]
 fn create_with_max_count_overflow() {
 	new_test_ext().execute_with(|| {
 		KittiesCount::<Test>::put(u32::max_value());
-		assert_noop!(Kitties::create(Origin::signed(1)), Error::<Test>::KittiesCountOverflow);
+		assert_noop!(new_kitty(1), Error::<Test>::KittiesCountOverflow);
 	});
 }
 
 #[test]
 fn create_test_success_with_event() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(Kitties::create(Origin::signed(1)));
+		assert_ok!(new_kitty(1));
 		assert_eq!(KittiesCount::<Test>::get(), Some(1));
-		System::assert_has_event(TestEvent::Kitties(Event::KittyCreated(1, 1)));
+		assert_event!(Event::KittyCreated(1, 1));
 	});
 }
 
@@ -24,7 +39,7 @@ fn create_test_success_with_event() {
 fn create_last_with_id_max_value() {
 	new_test_ext().execute_with(|| {
 		KittiesCount::<Test>::put(u32::max_value() - 1);
-		assert_ok!(Kitties::create(Origin::signed(1)));
+		assert_ok!(new_kitty(1));
 		assert_eq!(KittiesCount::<Test>::get(), Some(u32::max_value()));
 	});
 }
@@ -32,23 +47,23 @@ fn create_last_with_id_max_value() {
 #[test]
 fn create_failed_with_not_enough_balance() {
 	new_test_ext().execute_with(|| {
-		assert_noop!(Kitties::create(Origin::signed(3)), Error::<Test>::NotEnoughBalance);
+		assert_noop!(new_kitty(3), Error::<Test>::NotEnoughBalance);
 	});
 }
 
 #[test]
 fn transfer_success() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_ok!(Kitties::transfer(Origin::signed(1), 2, 1));
-		System::assert_has_event(TestEvent::Kitties(Event::KittyTransfered(1, 2, 1)));
+		assert_event!(Event::KittyTransfered(1, 2, 1));
 	});
 }
 
 #[test]
 fn transfer_fail_when_to_some_owner() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_noop!(Kitties::transfer(Origin::signed(1), 1, 1), Error::<Test>::SameOwner);
 	});
 }
@@ -56,7 +71,7 @@ fn transfer_fail_when_to_some_owner() {
 #[test]
 fn transfer_fail_not_owner() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_noop!(Kitties::transfer(Origin::signed(2), 1, 1), Error::<Test>::NotOwnerOfKitty);
 	});
 }
@@ -64,12 +79,12 @@ fn transfer_fail_not_owner() {
 #[test]
 fn breed_success() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
+		let _ = new_kitty(1);
 
 		assert_ok!(Kitties::breed(Origin::signed(1), 1, 2));
 		assert_eq!(KittiesCount::<Test>::get(), Some(3));
-		System::assert_has_event(TestEvent::Kitties(Event::KittyCreated(1, 3)));
+		assert_event!(Event::KittyCreated(1, 3));
 	});
 }
 
@@ -90,8 +105,8 @@ fn breed_fail_with_invalid_index() {
 #[test]
 fn breed_fail_with_invalid_owner() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
-		let _ = Kitties::create(Origin::signed(2));
+		let _ = new_kitty(1);
+		let _ = new_kitty(2);
 
 		assert_noop!(Kitties::breed(Origin::signed(1), 1, 2), Error::<Test>::NotOwnerOfKitty);
 	});
@@ -100,8 +115,8 @@ fn breed_fail_with_invalid_owner() {
 #[test]
 fn breed_fail_with_count_overflow() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
+		let _ = new_kitty(1);
 
 		KittiesCount::<Test>::put(u32::max_value());
 
@@ -122,29 +137,29 @@ fn sell_fail_with_not_owner() {
 #[test]
 fn sell_success() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_ok!(Kitties::sell(Origin::signed(1), 1, Some(100)));
-		System::assert_has_event(TestEvent::Kitties(Event::KittyForSale(1, 1, Some(100))));
+		assert_event!(Event::KittyForSale(1, 1, Some(100)));
 	});
 }
 
 #[test]
 fn cancel_sell_with_none_price() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		let _ = Kitties::sell(Origin::signed(1), 1, Some(100));
 
 		assert_ok!(Kitties::sell(Origin::signed(1), 1, None));
 
 		assert_eq!(None, KittiesPrice::<Test>::get(1));
-		System::assert_has_event(TestEvent::Kitties(Event::KittyCancelSale(1, 1)));
+		assert_event!(Event::KittyCancelSale(1, 1));
 	});
 }
 
 #[test]
 fn buy_failed_when_already_owned() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_noop!(Kitties::buy(Origin::signed(1), 1), Error::<Test>::KittyAlreadyOwned);
 	});
 }
@@ -152,7 +167,7 @@ fn buy_failed_when_already_owned() {
 #[test]
 fn buy_fail_when_not_for_sale() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		assert_noop!(Kitties::buy(Origin::signed(2), 1), Error::<Test>::NotForSale);
 	});
 }
@@ -160,7 +175,7 @@ fn buy_fail_when_not_for_sale() {
 #[test]
 fn buy_fail_with_not_enough_balance() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		let _ = Kitties::sell(Origin::signed(1), 1, Some(100));
 
 		assert_noop!(Kitties::buy(Origin::signed(3), 1), Error::<Test>::NotEnoughBalance);
@@ -170,13 +185,13 @@ fn buy_fail_with_not_enough_balance() {
 #[test]
 fn buy_success() {
 	new_test_ext().execute_with(|| {
-		let _ = Kitties::create(Origin::signed(1));
+		let _ = new_kitty(1);
 		let _ = Kitties::sell(Origin::signed(1), 1, Some(100));
 
 		assert_ok!(Kitties::buy(Origin::signed(2), 1));
 
 		assert_eq!(KittiesPrice::<Test>::contains_key(1), false);
 
-		System::assert_has_event(TestEvent::Kitties(Event::KittyTransfered(1, 2, 1)));
+		assert_event!(Event::KittyTransfered(1, 2, 1));
 	});
 }
